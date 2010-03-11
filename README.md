25a2e16074f929f3ae697c23405d6b08bd5c682d
Ted Nyman <tnm800@gmail.com>
2010-03-10 16:37:52 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+Vulcan
+======
+
+**The highly logical, ultra-simple way to populate Redis with data**
+
+Perhaps you want to populate Redis with a bunch of test data? Or maybe you want to try and reproduce a bug you're facing, and need to pour in a lot of random data?
+
+Well, you want Vulcan.
+
+So Simple, Even a Klingon Can Do It
+---------------------------------------
+
+Vulcan is written in Python, so all you'll need is [redis-py](http://github.com/andymccurdy/redis-py "redis-py").
+
+Put Vulcan in your PYTHONPATH and you're all set.
+
+What It Does
+---------------
+
+Vulcan will populate your Redis datastore with test data. It works for each of the built-in Redis data structures. It uses SET for strings, LPUSH for lists, SADD for sets, and ZADD for sorted sets. 
+
+Vulcan creates the test data on a seperate DB for each datatype: DB's 6, 7, 8, and 9, respectively.
+
+How To Do It
+-------------
+
+Say you want to SET ten thousand keys, with randomly generated values.
+
+Here's all you need to do:
+
+First, create a Vulcan object like so:
+
+	from vulcan import Vulcan
+
+	s = Vulcan(10000)
+
+The default is to populate with strings, but to be explicit you could do:
+
+	s = Vulcan(10000, 'strings')
+
+For lists, sets, or sorted sets:
+
+	lists = Vulcan(10000, 'lists')
+	sets = Vulcan(10000, 'sets')
+	zsets = Vulcan(1000, 'zsets')
+
+Now call your object like so:
+
+	s.populate()
+
+That will clear out database #6 and populate it with your random data. You're all set.
+
+The keys are given random integer key names, and the values and scores are also given random integers. Next version, I'll likely add in some other options for random values (probably Star Trek-based).
+
+That's all there is to it. You are now bursting with Redis data. Live long, and prosper. _\\//
---
+++ vulcan.py
@@ -0,0 +1,80 @@
+"""
+Vulcan: the highly logical way to pre-populate Redis
+
+Example:
+
+	from vulcan import Vulcan
+	s = Vulcan(10000, 'strings') # We want 10,000 random strings
+	s.populate() #Populate the database
+"""
+
+__author__ = 'Ted Nyman'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+
+import redis
+import random
+from random import randint
+
+class Vulcan(object):
+    """
+    Custom testing object from the future	
+    """
+    def __init__(self, size, datatype='strings'):
+        """
+        Size is an integer representing how many keys you want. 
+        Datatype can be 'strings', 'lists', 'sets', or 'zsets.' 
+        'Strings' is the default datatype.		
+        """
+        self.size = size		
+        self.datatype = datatype
+	
+    def populate(self):
+        """
+        Populate Redis with random values.
+        """
+        datatype = self.datatype
+	size = self.size
+
+        if type(size) == int:
+
+            if datatype == 'strings':
+		r = redis.Redis(host='localhost', port=6379, db=6)
+		r.flushdb()
+                bunch_o_keys = range(size)
+                for i in bunch_o_keys: 
+                    r.set(i, (randint(0, size)))
+
+            elif datatype == 'lists':
+		r = redis.Redis(host='localhost', port=6379, db=7)
+		r.flushdb()
+                bunch_o_keys = range(size)
+                for i in bunch_o_keys:
+                    r.lpush(i,(randint(0, size)))
+
+            elif datatype == 'sets':
+		r = redis.Redis(host='localhost', port=6379, db=8)
+		r.flushdb()
+                bunch_o_keys = range(size)
+                for i in bunch_o_keys:
+                    r.sadd(i,(randint(0, size)))
+
+            elif datatype == 'zsets':
+		r = redis.Redis(host='localhost', port=6379, db=9)
+		r.flushdb()
+                bunch_o_keys = range(size)
+                for i in bunch_o_keys:
+                    r.zadd(i,(randint(0, size)), (randint(0, size)))
+
+            else:
+                print "Error, can't populate. Options are 'strings', 'lists', 'sets', or 'zsets'. To do otherwise would be illogical."
+
+        else:
+	    print "Error, can't populate. You must specify an integer for the number of values." 
+ 
+
+
+
+
+			
+	
